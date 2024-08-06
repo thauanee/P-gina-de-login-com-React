9f27d485ee6f9cb193cd2a7e7c61c8b73c745d35
Thauane Louise <thauanelouise@MacBook-Air-de-Thauane.local>
2024-08-06 23:12:51 +0200

    Página de login com react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Login from '../Components/Login/Login'
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="App">
+    <Login />
+   </div>
+  )
+}
+
+export default App
